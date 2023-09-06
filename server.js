83009667be068c46ebd5c1e8a9a9375f00dabbb3
Harikrushna <32766262+hardikhari96@users.noreply.github.com>
2023-09-06 22:54:32 +0530

    Ejs implimented

--- server.js
+++ server.js
@@ -1,50 +1,14 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
 const app = express();
+
 const port = 3000;
 
 const podIP = process.env.POD_IP || 'IP not available';
 
-app.all('*',(req,res)=>{
-    const html = `
-    
-    <!DOCTYPE html>
-<html>
-<head>
-  <title>Your Site Hosted in Kubernetes</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      text-align: center;
-      margin: 20px;
-    }
-    h1 {
-      margin: 10px;
-    }
-    .social-links {
-      margin-top: 20px;
-    }
-    .social-link {
-      margin: 10px;
-      font-size: 18px;
-    }
-  </style>
-</head>
-<body>
-
-  <h1>Hello, This site is hosted in K8</h1>
-  <p>Pod IP: ${podIP}</p> <!-- Replace ${podIP} with the actual Pod IP using your server-side code -->
-
-  <div class="social-links">
-    <a class="social-link" href="https://github.com/hardikhari96" target="_blank">GitHub</a><br>
-    <a class="social-link" href="https://www.linkedin.com/in/hardikhari96/" target="_blank">LinkedIn</a><br>
-    <a class="social-link" href="https://stackoverflow.com/users/8823231/harikrushna-patel" target="_blank">Stack Overflow</a>
-  </div>
-
-</body>
-</html>
- 
-    
-    `;
+app.all('*',async (req,res)=>{
+    let html =  await ejs.renderFile(path.join(__dirname,'./views/index.html'),{podIP});
     res.send(html);
 })
 
